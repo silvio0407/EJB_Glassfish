0bd804aef11e385f05c09369d755ce2c1b5eabed
Silvio <silvio0407@gmail.com>
2019-07-24 16:08:45 +0100

    Projeto EJB

---
+++ src/mb/GrausMB.java
@@ -0,0 +1,37 @@
+package mb;
+
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import beans.GrausBean;
+
+@ManagedBean
+public class GrausMB {
+
+	@EJB
+	GrausBean grausBean;
+
+	double c;
+	double resultadoF;
+
+	public void converterParaFahrenheit() {
+		this.resultadoF = this.grausBean.converterParaFahrenheit(c);
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+	public double getResultadoF() {
+		return resultadoF;
+	}
+
+	public void setResultadoF(double resultadoF) {
+		this.resultadoF = resultadoF;
+	}
+}
